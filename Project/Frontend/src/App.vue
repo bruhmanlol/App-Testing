bf7e53eb79d1e2f06cfe473d2257e10ff017e551
anay-schottlabs <anay@schottlabs.com>
2025-11-06 18:57:22 -0800

    Fetch test message from local server

--- Project/Frontend/src/App.vue
+++ Project/Frontend/src/App.vue
@@ -1,11 +1,34 @@
 <script setup>
-import { ref } from "vue"
+import { ref, onMounted } from "vue"
 
-const data = ref("No data yet.")
+const message = ref("No data yet.")
+
+const url = "/test-message"
+
+async function fetchTestMessage() {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`)
+    }
+
+    const result = await response.json()
+
+    return result
+  }
+  catch (error) {
+    return error.message
+  }
+}
+
+onMounted(() => {
+  fetchTestMessage().then(response => message.value = response)
+})
 </script>
 
 <template>
-  <h1>Result: {{ data }}</h1>
+  <h1>Result: {{ message }}</h1>
 </template>
 
 <style scoped></style>
